e472d518cadeeb9955c141ff08ce9c56d66ee5d0
Sergey Krivov <sergey@krivov.info>
2014-06-03 20:09:31 +0300

    add test dump

---
+++ dump.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v10.42 
+MySQL - 5.6.16-log : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `colors` */
+
+DROP TABLE IF EXISTS `colors`;
+
+CREATE TABLE `colors` (
+  `color` varchar(255) NOT NULL,
+  PRIMARY KEY (`color`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `votes` */
+
+DROP TABLE IF EXISTS `votes`;
+
+CREATE TABLE `votes` (
+  `city` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `votes` float DEFAULT NULL,
+  PRIMARY KEY (`city`,`color`),
+  KEY `color_frn1` (`color`),
+  CONSTRAINT `color_frn1` FOREIGN KEY (`color`) REFERENCES `colors` (`color`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
