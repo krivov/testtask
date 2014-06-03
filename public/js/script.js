70846b7ebf02516fa5c3c22d558c5674dc0bb17e
Sergey Krivov <sergey@krivov.info>
2014-06-04 01:00:01 +0300

    sum total

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,9 @@ $(document).ready(function(){
             success:    function(data) {
                 if (data.votes || data.votes == 0) {
                     $td.html(data.votes);
+                    if (data.votes != 0) {
+                        $('.td-total').html('');
+                    }
                 }else {
                     alert('Something went wrong');
                 }
@@ -17,6 +20,26 @@ $(document).ready(function(){
                 alert('Something went wrong');
             }
         });
+        return false;
+    });
+
+    $(".totalLink").click(function () {
+        $td = $(this).parent().next('td');
+        $td.html('');
+
+        var sum = 0;
+        $('.votesTable tr td.td-votes').each(function( index ) {
+            console.log( sum );
+            console.log( parseInt($(this).html()) );
+            if (parseInt($(this).html()) > 0) {
+                sum = sum + parseInt($(this).html());
+            }
+            console.log( sum );
+            console.log( index + ": " + $( this ).text() );
+            console.log( sum );
+        });
+        $td.html(sum);
+
         return false;
     });
 });
